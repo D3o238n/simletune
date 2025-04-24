c81a4f57639da16dd0344795d3080b50cc940ed5
D_3on <danielanaram@gmail.com>
2025-04-24 23:18:21 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlJFjS26lVwsB7KHGenZPm43LcyhqKP-4",
-  authDomain: "simpletune-46c58.firebaseapp.com",
-  projectId: "simpletune-46c58",
-  storageBucket: "simpletune-46c58.firebasestorage.app",
-  messagingSenderId: "394871811733",
-  appId: "1:394871811733:web:c9cb2f8e38fc02c24ff59c",
-  measurementId: "G-JHG190019Q"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
